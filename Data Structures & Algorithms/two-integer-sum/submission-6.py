48801e3820a85670a738e025cdb5dfdee6a770c3
Michael Rejo <32908690+MichaelRejo1601@users.noreply.github.com>
2026-08-22 00:00:50 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,21 @@
+# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+
+# return the indeices, they cannot be the same
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+# return smaller one first
+
+# how can we get the target sum 
+
+# is the array sorted?
+ # no the array is not sorted
+# no dupes
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mem = {}
+        for i in range(len(nums)):
+            check = mem.get(target-nums[i], -1)
+            mem[nums[i]] = i
+            if check != -1 and check != i:
+                return [check, i]
