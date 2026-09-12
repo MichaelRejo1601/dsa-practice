af5c63dfce6386188a9470ffe0c519d261496008
Michael Rejo <32908690+MichaelRejo1601@users.noreply.github.com>
2026-09-12 11:37:41 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,55 @@
+# n cars traveling to the same destination on a high
+
+# you are given two int array position and speed of same length 
+# position of speed of the ith car
+
+# desination is a t target miles
+
+
+# a car cannot pass the car ahead of it, it can onyl drive up to another car and then drive at the same speed as it 
+
+# a car fleet is a non empty set of cars driving at the same position and same speed a single car is also considered a fleet
+
+# if a car catches up to the fleet the moment the fleet reaches the destination, it is considered part of the fleet
+
+#return the number of different car fleets that will arrive at the destination
+
+# number of cars is medium 
+# target is medium
+# speed is medium
+
+# speed is never 0 
+
+# speed can be greater than target
+
+
+#Input: target = 10, position = [1,4], speed = [3,2]
+
+# time to destination < time of next to destination, then it is part of the fleet 
+
+# sort the array first 
+
+# go backwards through it
+
+
+
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = []
+
+        for i in range(len(position)):
+            cars.append((position[i], speed[i]))
+        
+        cars.sort(reverse=True)
+        counter = 0
+        last_fleet_time_to_arrival = 0
+
+        for car in cars:
+            time_to_arrive = (target-car[0])/car[1]
+            if time_to_arrive > last_fleet_time_to_arrival:
+                counter += 1 
+                last_fleet_time_to_arrival = time_to_arrive
+
+        return counter
